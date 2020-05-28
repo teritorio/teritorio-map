4db2919d112259f12f03a6e1544749a0f6e26f6b
Guillaume AMAT <guillaume@amat.io>
2020-05-28 19:03:59 +0200

    refactor: add MapboxValues type

--- src/poiFilter.ts
+++ src/poiFilter.ts
@@ -1,7 +1,7 @@
 import mapboxgl from 'mapbox-gl';
 
 interface Options {
-  filter?: Array<any>;
+  filter?: MapboxValues;
   include?: boolean;
 }
 
@@ -14,10 +14,10 @@ export function poiFilter(map: mapboxgl.Map, options?: Options) {
   }
 
   return {
-    setExcludeFilter(filter: Array<any>) {
+    setExcludeFilter(filter: MapboxValues) {
       applyFilter(map, filter, false);
     },
-    setIncludeFilter(filter: Array<any>) {
+    setIncludeFilter(filter: MapboxValues) {
       applyFilter(map, filter, true);
     },
     unsetFilter() {
@@ -37,15 +37,15 @@ const poiLayers = [
 
 const filterId = ['==', ['literal', '__poiFilter'], '__poiFilter'];
 
-function pruneFilter(filter: any): any {
-  if (filter[0] !== 'all') {
+function pruneFilter(filter: MapboxValues) {
+  if (!filter || !Array.isArray(filter) || filter[0] !== 'all') {
     return undefined;
   }
 
   return filter.filter(
-    (close: any) =>
+    close =>
       !(
-        close instanceof Array &&
+        Array.isArray(close) &&
         close.length >= 2 &&
         close[0] === 'all' &&
         JSON.stringify(close[1]) === JSON.stringify(filterId)
@@ -53,15 +53,27 @@ function pruneFilter(filter: any): any {
   );
 }
 
-function createFilterExpression(filter: Array<any>): Array<any> {
-  const f1 = filter?.filter(f => f.length === 1).map(f => f[0]);
-  const f2 = filter?.filter(f => f.length === 2).map(f => f.join('|'));
-  const f3 = filter?.filter(f => f.length === 3).map(f => f.join('|'));
+function formatExpressionLiterals(filters: MapboxValues[], filterLength: number) {
+  return filters
+    .filter((f): f is MapboxValues[] => Array.isArray(f) && f.length === filterLength)
+    .map(f => (filterLength === 1 ? f[0] : f.join('|')));
+}
 
-  var expression: Array<any> = ['any'];
+function createFilterExpression(filter: MapboxValues) {
+  if (!filter || !Array.isArray(filter)) {
+    return undefined;
+  }
+
+  const f1 = formatExpressionLiterals(filter, 1);
+  const f2 = formatExpressionLiterals(filter, 2);
+  const f3 = formatExpressionLiterals(filter, 3);
+
+  const expression: MapboxValues = ['any'];
+
   if (f1.length > 0) {
     expression.push(['in', ['get', 'tourism_superclass'], ['literal', f1]]);
   }
+
   if (f2.length > 0) {
     expression.push([
       'in',
@@ -69,6 +81,7 @@ function createFilterExpression(filter: Array<any>): Array<any> {
       ['literal', f2],
     ]);
   }
+
   if (f3.length > 0) {
     expression.push([
       'in',
@@ -86,8 +99,13 @@ function createFilterExpression(filter: Array<any>): Array<any> {
   return expression;
 }
 
-function applyFilter(map: mapboxgl.Map, filter: Array<any>, include: boolean = true) {
-  let expression: any;
+function applyFilter(map: mapboxgl.Map, filter: MapboxValues, include: boolean = true) {
+  if (!filter || !Array.isArray(filter)) {
+    return;
+  }
+
+  let expression: MapboxValues;
+
   if (include) {
     expression = ['all', filterId, createFilterExpression(filter)];
   } else {
@@ -95,13 +113,12 @@ function applyFilter(map: mapboxgl.Map, filter: Array<any>, include: boolean = t
   }
 
   poiLayers.forEach(layerId => {
-    let styleFilter = map.getFilter(layerId);
-    styleFilter = pruneFilter(styleFilter);
+    const styleFilter = pruneFilter(map.getFilter(layerId));
+
     if (!styleFilter) {
-      console.log(`Cannot amend filter of layer "{layerId}"`);
+      console.warn(`Cannot amend filter of layer "${layerId}"`);
     } else {
       styleFilter.push(expression);
-      console.log(JSON.stringify(styleFilter));
       map.setFilter(layerId, styleFilter);
     }
   });
@@ -109,8 +126,8 @@ function applyFilter(map: mapboxgl.Map, filter: Array<any>, include: boolean = t
 
 export function unsetFilter(map: mapboxgl.Map) {
   poiLayers.forEach(layerId => {
-    let styleFilter = map.getFilter(layerId);
-    styleFilter = pruneFilter(styleFilter);
+    const styleFilter = pruneFilter(map.getFilter(layerId));
+
     if (!styleFilter) {
       console.warn(`Cannot amend filter of layer "${layerId}"`);
     } else {
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,1 +1,3 @@
 declare var __DEV__: boolean;
+
+type MapboxValues = undefined | string | number | boolean | null | MapboxValues[];
