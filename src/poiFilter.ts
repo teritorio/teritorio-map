5c62f8459ed7de965a6ae146693a57c85a20ccd2
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2020-06-29 22:52:01 +0200

    feat: picto filter (#18)
    * feat: switch icon to picto with filter #15
    
    * Merge icon into poiFilter

--- src/poiFilter.ts
+++ src/poiFilter.ts
@@ -3,6 +3,7 @@ import mapboxgl from 'mapbox-gl';
 interface Options {
   filter?: MapboxExpr;
   include?: boolean;
+  picto?: boolean;
 }
 
 export function poiFilter(map: mapboxgl.Map, options?: Options) {
@@ -11,45 +12,57 @@ export function poiFilter(map: mapboxgl.Map, options?: Options) {
   }
 
   return {
-    setExcludeFilter(filter: MapboxExpr) {
-      applyFilter(map, filter, false);
+    setExcludeFilter(filter: MapboxExpr, picto: boolean = true) {
+      applyFilter(map, filter, false, picto);
     },
-    setIncludeFilter(filter: MapboxExpr) {
-      applyFilter(map, filter, true);
+    setIncludeFilter(filter: MapboxExpr, picto: boolean = true) {
+      applyFilter(map, filter, true, picto);
     },
-    unsetFilter() {
+    remove(picto: boolean = true) {
+      applyFilter(map, [], true, picto);
+    },
+    reset() {
       unsetFilter(map);
     },
   };
 }
 
+// Redirection function
+
+function applyFilter(
+  map: mapboxgl.Map,
+  filter: MapboxExpr,
+  include: boolean = true,
+  picto: boolean = true,
+) {
+  if (!filter || !Array.isArray(filter)) {
+    return;
+  }
+
+  if (picto) {
+    unsetHideFilter(map);
+    applyPictoLayout(map, filter, include);
+  } else {
+    unsetPictoLayout(map);
+    applyHideFilter(map, filter, include);
+  }
+}
+
+function unsetFilter(map: mapboxgl.Map) {
+  unsetHideFilter(map);
+  unsetPictoLayout(map);
+}
+
 const poiLayers = [
+  'poi-level-1-picto',
+  'poi-level-2-picto',
+  'poi-level-3-picto',
   'poi-level-1',
-  'poi-level-1-icon',
   'poi-level-2',
-  'poi-level-2-icon',
   'poi-level-3',
-  'poi-level-3-icon',
 ];
 
 const filterId: MapboxExpr = ['let', '_bloc_name', 'poiFilter'];
-
-function pruneFilter(filter: MapboxExpr) {
-  if (!filter || filter[0] !== 'all') {
-    return undefined;
-  }
-
-  return filter.filter(
-    close =>
-      !(
-        Array.isArray(close) &&
-        close.length === filterId.length + 1 &&
-        close[0] === filterId[0] &&
-        close[1] === filterId[1] &&
-        close[2] === filterId[2]
-      ),
-  );
-}
 
 function formatExpressionLiterals(filters: MapboxExpr, filterLength: number) {
   return filters
@@ -93,11 +106,26 @@ function createFilterExpression(filter: MapboxExpr) {
   return expression;
 }
 
-function applyFilter(map: mapboxgl.Map, filter: MapboxExpr, include: boolean = true) {
-  if (!filter || !Array.isArray(filter)) {
-    return;
+// Hide POI
+
+function pruneHideFilter(filter: MapboxExpr) {
+  if (!filter || filter[0] !== 'all') {
+    return undefined;
   }
 
+  return filter.filter(
+    close =>
+      !(
+        Array.isArray(close) &&
+        close.length === filterId.length + 1 &&
+        close[0] === filterId[0] &&
+        close[1] === filterId[1] &&
+        close[2] === filterId[2]
+      ),
+  );
+}
+
+function applyHideFilter(map: mapboxgl.Map, filter: MapboxExpr, include: boolean = true) {
   let expression: MapboxExpr;
 
   if (include) {
@@ -107,7 +135,7 @@ function applyFilter(map: mapboxgl.Map, filter: MapboxExpr, include: boolean = t
   }
 
   poiLayers.forEach(layerId => {
-    const styleFilter = pruneFilter(map.getFilter(layerId));
+    const styleFilter = pruneHideFilter(map.getFilter(layerId));
 
     if (!styleFilter) {
       console.warn(`Cannot amend filter of layer "${layerId}"`);
@@ -118,14 +146,88 @@ function applyFilter(map: mapboxgl.Map, filter: MapboxExpr, include: boolean = t
   });
 }
 
-function unsetFilter(map: mapboxgl.Map) {
+function unsetHideFilter(map: mapboxgl.Map) {
   poiLayers.forEach(layerId => {
-    const styleFilter = pruneFilter(map.getFilter(layerId));
+    const styleFilter = pruneHideFilter(map.getFilter(layerId));
 
     if (!styleFilter) {
       console.warn(`Cannot amend filter of layer "${layerId}"`);
     } else {
       map.setFilter(layerId, styleFilter);
     }
+  });
+}
+
+// Switch to picto
+
+const filterPictoId: MapboxExpr = ['let', '_bloc_name', 'styleFilter'];
+const defaultStyle: MapboxExpr = ['get', 'tourism_style'];
+
+function ammendPictoFilter(filter: MapboxExpr, styleExpression: MapboxExpr) {
+  if (!filter || filter[0] !== 'all') {
+    return undefined;
+  }
+
+  const close = filter.find(
+    close =>
+      Array.isArray(close) &&
+      close.length === filterPictoId.length + 1 &&
+      close[0] === filterPictoId[0] &&
+      close[1] === filterPictoId[1] &&
+      close[2] === filterPictoId[2],
+  ) as MapboxExpr;
+
+  if (close && close[3] && (close[3] as MapboxExpr)[2]) {
+    (close[3] as MapboxExpr)[2] = styleExpression;
+  }
+
+  return filter;
+}
+
+function resetPictoLayout(expr: MapboxExpr) {
+  if (!expr || expr[0] !== 'let') {
+    return undefined;
+  }
+
+  expr[2] = defaultStyle;
+
+  return expr;
+}
+
+function applyPictoLayout(map: mapboxgl.Map, filter: MapboxExpr, include: boolean = true) {
+  let expression: MapboxExpr;
+
+  if (include) {
+    expression = filterId.concat([['!', createFilterExpression(filter)]]);
+  } else {
+    expression = filterId.concat([createFilterExpression(filter)]);
+  }
+
+  poiLayers.forEach(layerId => {
+    const layout = map.getLayoutProperty(layerId, 'icon-image');
+    const styleExpression = ['case', expression, '•', defaultStyle];
+
+    if (!layout) {
+      console.warn(`Cannot amend filter of layer "${layerId}"`);
+    } else {
+      layout[2] = styleExpression;
+      map.setLayoutProperty(layerId, 'icon-image', layout);
+    }
+
+    map.setFilter(layerId, ammendPictoFilter(map.getFilter(layerId), styleExpression));
+  });
+}
+
+function unsetPictoLayout(map: mapboxgl.Map) {
+  poiLayers.forEach(layerId => {
+    const layout = resetPictoLayout(map.getLayoutProperty(layerId, 'icon-image'));
+
+    if (!layout) {
+      console.warn(`Cannot amend layout of layer "${layerId}"`);
+    } else {
+      map.setLayoutProperty(layerId, 'icon-image', layout);
+    }
+
+    map.setFilter(layerId, ammendPictoFilter(map.getFilter(layerId), defaultStyle));
   });
 }
